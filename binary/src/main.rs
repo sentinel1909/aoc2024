41358c4cd0d5d7795b14807914ebdd6deb160f0f
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-11-30 08:56:28 -0800

    improvement: add cli args and file handling

--- binary/src/main.rs
+++ binary/src/main.rs
@@ -1,5 +1,29 @@
 // binary/src/main.rs
 
+// dependencies
+use clap::Parser;
+use day1::day1_challenge;
+use std::path::PathBuf;
+
+// struct type to represent the command line arguments
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    day: u8,
+    #[arg(short, long)]
+    input_file: String,
+}
+
 fn main() {
     println!("Hello, Advent of Code 2024!");
+
+    let args = Args::parse();
+    let raw_path = format!("../../input/{}", args.input_file);
+    let path = PathBuf::from(raw_path);
+
+    match args.day {
+        1 => day1_challenge(path),
+        _ => eprintln!("Invalid challenge name, please try again..."),
+    }
 }
--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,1 +1,16 @@
 // day1/src/lib.rs
+
+// dependencies
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+// function which solves the Day 1 challenge
+pub fn day1_challenge(input: PathBuf) {
+    println!("Day 1 Challenge");
+    let mut buf = String::new();
+    File::open(input).unwrap().read_to_string(&mut buf).unwrap();
+    println!("{}", buf);
+
+    println!("Answer:");
+}
