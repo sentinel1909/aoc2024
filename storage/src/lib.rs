9d482a1f1fa6e38975785ed3567d88c786390e2a
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-11-29 22:52:31 -0800

    improvement: implement read method on DalService

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -5,11 +5,10 @@ use opendal::Buffer;
 use opendal::Error;
 use opendal::Operator;
 use opendal::Result;
-use opendal::services::Fs;
 
 // trait definitions
 trait Dal {
-  fn read(&self) -> Result<Buffer, Error>; 
+  async fn read(&self) -> Result<Buffer, Error>; 
 }
 
 // struct type to represent an OpenDal service
@@ -18,6 +17,7 @@ struct DalService {
 }
 
 // implement the Dal trait for the DalService
+
 impl Dal for DalService {
   async fn read(&self) -> Result<Buffer> {
     let bytes = self.op.read("puzzle_data/").await?;
